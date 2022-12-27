18d5256b0e3e7c1b6c500a96502d5ab6ff5e54e4
Axel Froborg <axel.froborg@gmail.com>
2022-12-27 20:05:01 +0100

    add robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
   site: process.env.DEPLOY_URL
     ? `https://${process.env.DEPLOY_URL}`
     : 'http://localhost:3000',
-  integrations: [sitemap(), image(), tailwind()],
+  integrations: [sitemap(), image(), tailwind(), robotsTxt()],
 });
