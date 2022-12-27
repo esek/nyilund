9d009197edd1d6e9f860b32d608ee7cc3aaac23d
Axel Froborg <axel.froborg@gmail.com>
2022-12-27 18:20:22 +0100

    refactor pages

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,2 +1,11 @@
-export { default as en } from './en.json';
-export { default as sv } from './sv.json';
+import type { Locale } from '@/models/i18n';
+import get from 'lodash/get';
+
+import { default as en } from './en.json';
+import { default as sv } from './sv.json';
+
+export const getTranslations = (locale: Locale) => {
+  return (key: string) => {
+    return get({ en, sv }, `${locale}.${key}`) ?? key;
+  };
+};
